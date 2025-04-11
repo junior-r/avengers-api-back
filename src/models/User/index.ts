c9843903c9a63b0f95b152b75b59896ac72a8464
junior31064049@gmail.com <junior31064049@gmail.com>
2025-04-11 17:00:00 -0500

    the omit clause has been added for password field in the method getById

--- src/models/User/index.ts
+++ src/models/User/index.ts
@@ -1,19 +1,7 @@
-import { PrismaClient, User } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 
 const prisma = new PrismaClient()
-
-const returnUserInfo = (user: User) => {
-    return {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        lastName: user.lastName,
-        isMod: user.isMod,
-        isAdmin: user.isAdmin,
-        isActive: user.isActive
-    }
-}
 
 export class UserModel {
     static async getById({ id }: { id: string }) {
@@ -21,12 +9,15 @@ export class UserModel {
             const user = await prisma.user.findUnique({
                 where: {
                     id: id
+                },
+                omit: {
+                    password: true
                 }
             })
             if (!user) {
                 throw new Error("User not found")
             }
-            return returnUserInfo(user)
+            return user
         } catch (_error) {
             throw new Error("Error getting user by id")
         }
